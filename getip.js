c746e727269ea7e1506a025fb64dd67592c517b5
Cezary <cezarypi5@users.noreply.github.com>
2026-03-10 17:57:52 +0000

    Remove temporary debug files, upgrade system prompt to full Claude power

--- getip.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-const nets = os.networkInterfaces();
-Object.keys(nets).forEach(function(name) {
-  nets[name].forEach(function(addr) {
-    if (addr.family === 'IPv4' && !addr.internal) {
-      console.log(name + ': ' + addr.address);
-    }
-  });
-});
